19a582d2a00b79218fc715a20f9e143608f6fd41
Rob Nichols <rob.nichols@buildwithbmc.com>
2019-08-27 14:25:28 -0600

    starting framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
