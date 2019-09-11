a2a197b01809329816cf651b5dfbd68260fa9f80
Rob Nichols <rob.nichols@buildwithbmc.com>
2019-09-11 11:11:53 -0600

    more contact form updates

--- src/app/contact-request/contact-request.component.ts
+++ src/app/contact-request/contact-request.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'contact-request',
-    templateUrl: './contact-request.component.html'
+    templateUrl: './contact-request.component.html',
+    styleUrls: ['./contact-request.component.css']
 })
 
 export class ContactRequestComponent {
-    contactName = new FormControl('');
+    contactForm = new FormGroup({
+        contactName: new FormControl(''),
+        contactEmail: new FormControl(''),
+        requestType: new FormControl(''),
+        requestMessage: new FormControl('')
+    });
+    
 }
